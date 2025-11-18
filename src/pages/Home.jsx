596aa3fc169c4c5101b5a07bc3926f6465ae6208
maxianguita <maxi.7anguita@gmail.com>
2025-11-18 01:12:11 -0300

    Fix: imports y estructura Home para Vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 // src/pages/Home.jsx
 import { useState, useEffect } from "react";
-import CustomerForm from "../components/CustomerForm";
-import SizeSelector from "../components/SizeSelector";
-import FlavorSelector from "../components/FlavorSelector";
-import ConfirmOrder from "../components/ConfirmOrder";
-import Navbar from "../components/Navbar.jsx";
 
-import MapModal from "../components/MapModal";
-import SideMenu from "../components/SideMenu";
-import PanelControl from "../components/PanelControl";
+// Components
+import CustomerForm from "../components/CustomerForm";
+import SizeSelector from "../components/SizeSelector.jsx";
+import FlavorSelector from "../components/FlavorSelector.jsx";
+import ConfirmOrder from "../components/ConfirmOrder.jsx";
+import Navbar from "../components/Navbar.jsx";
+import MapModal from "../components/MapModal.jsx";
+import SideMenu from "../components/SideMenu.jsx";
+import PanelControl from "../components/PanelControl.jsx";
+
+// Assets
 import Logo from "../assets/arti.png";
 
 // ---- Estilos ---- 
@@ -28,7 +31,7 @@ const styles = {
   logoWrapper: {
     textAlign: "center",
     marginBottom: "25px",
-    marginTop: "90px",       
+    marginTop: "90px",
   },
   logoImg: {
     width: "160px",
@@ -39,7 +42,7 @@ const styles = {
   },
 };
 
- function Home() {
+function Home() {
   const [customer, setCustomer] = useState({ name: "", phone: "" });
   const [size, setSize] = useState(null);
   const [maxFlavors, setMaxFlavors] = useState(0);
@@ -50,15 +53,14 @@ const styles = {
   const [currentPage, setCurrentPage] = useState("Order");
   const [availableFlavors, setAvailableFlavors] = useState([]);
 
-useEffect(() => {
-  fetch("http://localhost:4000/api/flavors")
-    .then((res) => res.json())
-    .then((data) => {
-      //console.log("Sabores cargados:", data); 
-      setAvailableFlavors(data.map((f) => f.name));
-    })
-    .catch((err) => console.error("Error cargando sabores:", err));
-}, []);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/flavors")
+      .then((res) => res.json())
+      .then((data) => {
+        setAvailableFlavors(data.map((f) => f.name));
+      })
+      .catch((err) => console.error("Error cargando sabores:", err));
+  }, []);
 
   const handleSelectSize = (val) => {
     setSize(val);
@@ -86,7 +88,6 @@ useEffect(() => {
 
   return (
     <div style={styles.appContainer}>
-
       <Navbar
         navigate={handleNavigate}
         currentPage={currentPage}
@@ -95,31 +96,32 @@ useEffect(() => {
       />
 
       <div style={styles.container}>
-
         <div style={styles.logoWrapper}>
           <img src={Logo} alt="Arti Helados" style={styles.logoImg} />
         </div>
 
         <CustomerForm customer={customer} setCustomer={setCustomer} />
+
         <SizeSelector size={size} handleSelectSize={handleSelectSize} />
 
-       {size && (
-  <FlavorSelector
-    selected={flavors}
-    toggle={toggleFlavor}
-    max={maxFlavors}
-    flavorsList={availableFlavors}   
-    sectionStyle={styles.sectionBase}
-  />
-)}
-
+        {size && (
+          <FlavorSelector
+            selected={flavors}
+            toggle={toggleFlavor}
+            max={maxFlavors}
+            flavorsList={availableFlavors}
+          />
+        )}
 
         {size && flavors.length === maxFlavors && (
           <ConfirmOrder customer={customer} size={size} flavors={flavors} />
         )}
       </div>
 
-      <MapModal isVisible={showMapModal} onClose={() => setShowMapModal(false)} />
+      <MapModal
+        isVisible={showMapModal}
+        onClose={() => setShowMapModal(false)}
+      />
 
       <SideMenu
         open={menuOpen}
@@ -128,11 +130,9 @@ useEffect(() => {
         navigate={handleNavigate}
       />
 
-      {showPanel && (
-        <PanelControl onClose={() => setShowPanel(false)} />
-      )}
-
+      {showPanel && <PanelControl onClose={() => setShowPanel(false)} />}
     </div>
   );
 }
+
 export default Home;
