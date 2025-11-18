663b428898cd6bc6f0b9a6c5c81f483d25b97068
maxianguita <maxi.7anguita@gmail.com>
2025-11-18 14:51:31 -0300

    Protección de rutas con ProtectedRoute y refactor del App.jsx para login obligatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import PanelControl from "./components/PanelControl";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+
+        {/* LOGIN → Única ruta pública */}
         <Route path="/login" element={<Login />} />
 
-        <Route path="/panel" element={<PanelControl />} />
+        {/* RUTAS PROTEGIDAS */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/panel"
+          element={
+            <ProtectedRoute>
+              <PanelControl />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Cualquier ruta desconocida manda a login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+
       </Routes>
     </BrowserRouter>
   );
