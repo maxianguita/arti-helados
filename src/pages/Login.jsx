5423ed86de15b9aae6eb90dfc936df530248ba91
maxianguita <maxi.7anguita@gmail.com>
2025-11-21 13:58:00 -0300

    Fix API_URL console log y prueba de variable

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL:", API_URL);
 
 function Login() {
   const navigate = useNavigate();
